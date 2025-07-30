1c5b02ef52ed78e3439a9d8cdb11651b2373e9c5
Stev51 <dinosaurrampage51@gmail.com>
2025-07-30 16:22:15 -0500

    Setup visuals
    - Added world scene with background tile layer and blank main tile layer
    - Fixed camera stuff how I like it

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-const SPEED = 500
+const SPEED = 150
 
 func _physics_process(delta):
-	player_movement(delta)
+	player_movement()
 
-func player_movement(delta):
+func player_movement():
 	
 	var vec = Vector2.ZERO
 	vec.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
