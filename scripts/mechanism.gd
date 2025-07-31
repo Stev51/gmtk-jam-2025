db7db01a490ab5c15ca0e9537dc5e9e294d55c08
tjones <trent@trentjones.com>
2025-07-30 22:41:16 -0400

    TileEntityProperties custom resource

--- scripts/mechanism.gd
+++ scripts/mechanism.gd
@@ -1,9 +1,9 @@
 extends Node2D
+class_name Mechanism
 
-# Consider moving anything to custom resources
-# Walls and UI will be non-mechanisms, a different Node2D and Control respectively
+@export var properties : TileEntityProperties
+# Walls can be non-mechanisms with TileEntityproperties
 
-#Properties needed: walkable; movable (pickup-able/shovable) (i.e. false for in/out conveyors); 
 
 #Methods needed: detect player hands collision -> link incoming signals; push/pull signal -> move;
 #detect box collision -> link outgoing signals; clock signal -> perform operation on linked object;
---
+++ scripts/tileentityproperties.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name TileEntityProperties
+
+@export var walkable = false
+@export var movable_by_player = false #can be picked up, pulled, or pushed
+@export var movable_by_transporters = false #conveyor belts
