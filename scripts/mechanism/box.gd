e9d8cba262d2a2c64142d075cf5a9df9a8aacc86
Isaac Mango <bigmangohead@gmail.com>
2025-07-31 23:37:18 -0500

    Add colored boxes and painter script

--- scripts/mechanism/box.gd
+++ scripts/mechanism/box.gd
@@ -9,3 +9,11 @@ var color = BoxColor.WHITE
 func _init(field: Field, x: int, y: int):
 	super(field, x, y, NODE.instantiate())
 	field.deferBackgroundMechanismUpdate(Vector2i(x, y))
+	
+func push(directionToMove: Util.Direction) -> bool:
+	self.updateColor(Box.BoxColor.RED)
+	return super.push(directionToMove)
+
+func updateColor(color: BoxColor):
+	self.color = color
+	
---
+++ scripts/mechanism/painter.gd
@@ -0,0 +1,12 @@
+class_name Painter extends Mechanism
+
+const NODE = preload("res://scenes/pusher.tscn")
+
+func _init(field: Field, x: int, y:int):
+	super(field, x, y, NODE.instantiate())
+	
+func update(currentCycle: int):
+	pass
+	
+func push(directionToMove: Util.Direction) -> bool:
+	return false
