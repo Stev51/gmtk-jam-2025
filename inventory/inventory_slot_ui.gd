3cb97521e55345af71b14cf0ada876e95ae25140
Stev51 <dinosaurrampage51@gmail.com>
2025-08-01 00:06:20 -0500

    Inventory progress
    Continuing to follow a YouTube tutorial from a man who cannot pronounce 8th grade reading level words

--- inventory/inventory_slot_ui.gd
+++ inventory/inventory_slot_ui.gd
@@ -2,9 +2,9 @@ extends Panel
 
 @onready var item_visual: Sprite2D = $CenterContainer/Panel/ItemDisplay
 
-func update_display(item: InventoryItem):
-	if !item:
+func update_display(slot: InventorySlot):
+	if !slot:
 		item_visual.visible = false
 	else:
 		item_visual.visible = true
-		item_visual.texture = item.texture
+		item_visual.texture = slot.item.texture
