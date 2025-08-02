6f80ec854378055bec9ce7c8dc6e2311ed681308
Stev51 <dinosaurrampage51@gmail.com>
2025-08-02 01:42:38 -0500

    Add/connect blocker inv item

--- scripts/mechanism/blocker.gd
+++ scripts/mechanism/blocker.gd
@@ -1,7 +1,7 @@
 class_name Blocker extends Mechanism
 
 const NODE = preload("res://scenes/blocker.tscn")
-const ITEM = preload("res://inventory/items/combiner_item.tres")
+const ITEM = preload("res://inventory/items/blocker_item.tres")
 
 func _init(field: Field, x: int, y:int):
 	super(field, x, y, NODE.instantiate(), Field.FOREGROUND, ITEM)
