673426afcd6e4e2e6a466ce64ff0ebd76c07030f
MaxDaCat <maxneed@comcast.net>
2025-08-03 06:01:26 -0400

    added sfx for menu buttons

--- scripts/pause_screen.gd
+++ scripts/pause_screen.gd
@@ -8,8 +8,17 @@ func _input(event):
 			Global.resume_game.emit()
 
 func _on_resume_button_pressed():
+	$button.play()
 	Global.resume_game.emit()
 
 func _on_title_button_pressed():
+	$button.play()
 	Global.resume_game.emit()
 	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
+
+func _on_resume_button_mouse_entered() -> void:
+	$hover.play()
+
+
+func _on_title_button_mouse_entered() -> void:
+	$hover.play()
--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,8 +1,17 @@
 extends Node2D
 
 func _on_start_button_pressed():
+	$CenterContainer/button.play()
 	Global.resume_game.emit()
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func _on_quit_button_pressed():
+	$"CenterContainer2/door click".play()
+	await get_tree().create_timer(0.5).timeout
 	get_tree().quit()
+
+func _on_start_button_mouse_entered() -> void:
+	$hover.play()
+
+func _on_quit_button_mouse_entered() -> void:
+	$hover.play()
