d3676e9d8f82c2ec3ddc9cf1dfec5681e2768261
Stev51 <dinosaurrampage51@gmail.com>
2025-08-03 08:51:48 -0500

    Revert "Smooth camera movement"
    This reverts commit 1e5f5d27b3dfdb69e79ea688319748db66dc24c1.

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var gui_node = $GUI
+@onready var gui_node = $Player/GUI
 @onready var world_node = $World
 @onready var main_tile_map_layer = $World/MainTileMapLayer
 @onready var mechanisms_parent_node = $World/Mechanisms
@@ -9,9 +9,6 @@ extends Node2D
 
 var mouse_pos = Vector2.ZERO
 var cell_pos = Vector2.ZERO
-
-const PLAYER_MAX_VEC = Vector2(432, 480)
-const CAMERA_MAX_VEC = Vector2(160, 328)
 
 func _process(delta):
 
@@ -22,8 +19,6 @@ func _process(delta):
 	place_marker.pos_dist_x = float(place_marker.position.x) - float(player_node.position.x)
 	place_marker.pos_dist_y = float(place_marker.position.y) - float(player_node.position.y)
 	cell_pos -= world_node.TILE_OFFSET
-	
-	gui_node.position = (player_node.position / PLAYER_MAX_VEC) * CAMERA_MAX_VEC
 
 func _input(event):
 
