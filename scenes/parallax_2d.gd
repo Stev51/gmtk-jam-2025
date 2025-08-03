f18d664ca44617cd0a359dc4d2c4a4aa2be5f669
Stone <samstone9001@gmail.com>
2025-08-03 01:46:47 -0400

    re-add randomized star movement

--- scenes/parallax_2d.gd
+++ scenes/parallax_2d.gd
@@ -2,6 +2,6 @@ extends Parallax2D
 
 # randomizes star movement
 func _ready():
-	var speed = randfn(6, 1.5)
+	var speed = randfn(12, 2)
 	var angle = randf() * PI * 2
 	self.autoscroll = Vector2(speed * cos(angle), speed * sin(angle))
